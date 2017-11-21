f4888285a509f52de8128d1bfea6a08c45fb0e24
Nikki1550 <32400853+Nikki1550@users.noreply.github.com>
2017-11-21 16:26:03 -0500

    Color Guesser
    it does something

--- c1_7_favorite_color.py
+++ c1_7_favorite_color.py
@@ -3,3 +3,16 @@
 # Python Beginnings
 # 7 Favorite Color
 
+my_favorite_color = "transparent"
+
+user_color = input ("Try to guess what my favorite color is! ")
+
+while user_color != my_favorite_color:
+
+    print("That is the worst color ever lelelelel guess again")
+    user_color = input ("Try to guess what my favorite color is! ")
+
+else:
+    print("What a coincedence!")
+    
+     
